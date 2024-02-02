dc270d051add984cfe97cd77fb88f2af0da3443b
Stinny <Stinny.by@gmail.com>
2024-02-02 22:55:44 +0300

    Initial commit

---
+++ src/test/java/com/example/gensteeb/GensteebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gensteeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GensteebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
